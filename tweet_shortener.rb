0b20e89673052f53670a9ac0409c28a25e9cad2b
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-04 18:41:07 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  new_tweet = tweet.split(" ").map do |word|
+    dictionary.keys.each do |key|
+      if key.include?(word)
+        word = dictionary[key]
+      end
+    end
+    word
+  end
+  new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  shortened_tweets = tweet.map { |tweet| word_substituter(tweet)  }
+
+  shortened_tweets.each { |tweet| puts tweet  }
+end
+
+def selective_tweet_shortener(tweet)
+
+end
+
+def shortened_tweet_truncator(tweet)
+
+end
