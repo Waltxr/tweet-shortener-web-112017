ddf52bd418ecb61296e1985e3b87d9ed27cff1ad
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-04 18:44:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,7 @@ end
 def bulk_tweet_shortener(tweet)
   shortened_tweets = tweet.map { |tweet| word_substituter(tweet)  }
 
-  shortened_tweets.each { |tweet| puts tweet.split()  }
+  shortened_tweets.each { |tweet| puts tweet  }
 end
 
 def selective_tweet_shortener(tweet)
