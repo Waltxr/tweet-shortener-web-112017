bc957ac51e65ff4a89dfdf799e57e52f2c58774c
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-04 19:44:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ end
 def selective_tweet_shortener(tweet)
     if tweet.length > 140
       tweet = word_substituter(tweet)
-    elsif tweet < 130
+    elsif tweet.length < 130
       tweet = tweet
     end
   tweet
